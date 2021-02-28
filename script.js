7c5858bfda41fc28cfe26113901f96ba283d01ab
Solange Righi <slngvitoria@gmail.com>
2021-02-28 17:01:32 -0300

    adicionado relogio

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function aviso() {
 function corfundo(cor) {
   document.getElementById("body").style.backgroundColor = cor
 }
+
+function atualizaRelogio() {
+  document.getElementById("hora").innerHTML = new Date().toLocaleString()
+}
+setInterval(atualizaRelogio, 200);
+
